a7e392ef9d400eb7d09d588307819cd963315e54
owoPeef <owopeef@gmail.com>
2022-01-08 23:05:30 +0300

    Добавлена поддержка PlaceholdersAPI

--- src/ru/owopeef/chatandtabmanager/utils/Messages.java
+++ src/ru/owopeef/chatandtabmanager/utils/Messages.java
@@ -1,9 +1,39 @@
 package ru.owopeef.chatandtabmanager.utils;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import ru.tehkode.permissions.PermissionUser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Messages {
+    public static boolean isEvenlyDivisable(int a, int b) {
+        return a % b == 0;
+    }
+    
     public static String formatMessage(String original_message, PermissionUser user) {
+        List<String> list = new ArrayList<>();
+        String[] ss1 = original_message.split("\n");
+        for (String s1 : ss1) {
+            String[] ss2 = s1.split("%");
+            System.out.println(ss1.length);
+            if (isEvenlyDivisable(ss1.length, 2) || ss1.length == 3) {
+                int i = 0;
+                for (String s2 : ss2) {
+                    i++;
+                    if (i == 2) {
+                        i = 0;
+                        String strToAdd = "%" + s2 + "%:" + PlaceholderAPI.setPlaceholders(user.getPlayer(), "%" + s2 + "%");
+                        System.out.println(strToAdd);
+                        list.add(strToAdd);
+                    }
+                }
+                for (String l : list) {
+                    String[] split = l.split(":");
+                    original_message = original_message.replace(split[0], split[1]);
+                }
+            }
+        }
         return original_message
                 .replace("{player_suffix}", user.getSuffix()
                         .replace("&", "§"))
@@ -13,6 +43,28 @@ public class Messages {
     }
 
     public static String formatMessage(String original_message, boolean replace_colors, PermissionUser user) {
+        List<String> list = new ArrayList<>();
+        String[] ss1 = original_message.split("\n");
+        for (String s1 : ss1) {
+            String[] ss2 = s1.split("%");
+            System.out.println(ss1.length);
+            if (isEvenlyDivisable(ss1.length, 2) || ss1.length == 3) {
+                int i = 0;
+                for (String s2 : ss2) {
+                    i++;
+                    if (i == 2) {
+                        i = 0;
+                        String strToAdd = "%" + s2 + "%:" + PlaceholderAPI.setPlaceholders(user.getPlayer(), "%" + s2 + "%");
+                        System.out.println(strToAdd);
+                        list.add(strToAdd);
+                    }
+                }
+                for (String l : list) {
+                    String[] split = l.split(":");
+                    original_message = original_message.replace(split[0], split[1]);
+                }
+            }
+        }
         if (replace_colors) {
             return original_message
                     .replace("&", "§")
