96e7f0ebb56d65e02b855718e011f980fc94ddcb
owoPeef <owopeef@gmail.com>
2022-01-08 20:04:37 +0300

    Также Issues #1

---
+++ src/ru/owopeef/chatandtabmanager/utils/Messages.java
@@ -0,0 +1,44 @@
+package ru.owopeef.chatandtabmanager.utils;
+
+import ru.tehkode.permissions.PermissionUser;
+
+public class Messages {
+    public static String formatMessage(String original_message, PermissionUser user) {
+        return original_message
+                .replace("{player_suffix}", user.getSuffix()
+                        .replace("&", "§"))
+                .replace("{player_prefix}", user.getPrefix()
+                        .replace("&", "§"))
+                .replace("{player_nickname}", user.getName());
+    }
+
+    public static String formatMessage(String original_message, boolean replace_colors, PermissionUser user) {
+        if (replace_colors) {
+            return original_message
+                    .replace("&", "§")
+                    .replace("{player_suffix}", user.getSuffix()
+                            .replace("&", "§"))
+                    .replace("{player_prefix}", user.getPrefix()
+                            .replace("&", "§"))
+                    .replace("{player_nickname}", user.getName());
+        } else {
+            return original_message
+                    .replace("{player_suffix}", user.getSuffix()
+                            .replace("&", "§"))
+                    .replace("{player_prefix}", user.getPrefix()
+                            .replace("&", "§"))
+                    .replace("{player_nickname}", user.getName());
+        }
+    }
+
+    public static String formatMessage(String original_message, String message, PermissionUser user) {
+        return original_message
+                .replace("&", "§")
+                .replace("{player_suffix}", user.getSuffix()
+                        .replace("&", "§"))
+                .replace("{player_prefix}", user.getPrefix()
+                        .replace("&", "§"))
+                .replace("{player_message}", message)
+                .replace("{player_nickname}", user.getName());
+    }
+}
