04ec9e5948ac1e522a9aba3d8ccd3d04d6d194a3
owoPeef <owopeef@gmail.com>
2022-01-09 03:56:27 +0300

    Фикс багов

--- src/ru/owopeef/chatandtabmanager/Main.java
+++ src/ru/owopeef/chatandtabmanager/Main.java
@@ -12,20 +12,44 @@ public class Main extends JavaPlugin
     public void onEnable()
     {
         PluginManager pm = getServer().getPluginManager();
-        if (pm.isPluginEnabled("PermissionsEx") && pm.isPluginEnabled("PlaceholderAPI")) {
-            Config.loadConfig();
-            getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
-            for (Map.Entry<String, Map<String, Object>> cmd : getDescription().getCommands().entrySet()) {
-                String command = cmd.getKey();
-                try {
-                    getCommand(command).setExecutor(new Commands());
-                    this.getLogger().info("Command \"" + command + "\" init success");
-                } catch (Exception exc) {
-                    this.getLogger().warning("Command \"" + command + "\" don't init. ("+exc.getMessage()+")");
-                }
-            }
-        } else {
-            this.setEnabled(false);
+        int both = 0;
+
+        if (pm.getPlugin("PermissionsEx") != null) {
+            both = 2;
         }
+
+        if (pm.getPlugin("PlaceholderAPI") != null) {
+            if (both == 0) {
+                disablePlugin("PermissionsEx not installed.");
+            } else {
+                both = 4;
+                getLogger().info("PermissionsEx hooked.");
+                getLogger().info("PlaceholderAPI hooked.");
+                enablePlugin();
+            }
+        }
+
+        if (both == 2) {
+            disablePlugin("PlaceholderAPI not installed.");
+        }
+    }
+
+    public void enablePlugin() {
+        Config.loadConfig();
+        getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
+        for (Map.Entry<String, Map<String, Object>> cmd : getDescription().getCommands().entrySet()) {
+            String command = cmd.getKey();
+            try {
+                getCommand(command).setExecutor(new Commands());
+                this.getLogger().info("Command \"" + command + "\" init success");
+            } catch (Exception exc) {
+                this.getLogger().warning("Command \"" + command + "\" don't init. ("+exc.getMessage()+")");
+            }
+        }
+    }
+
+    public void disablePlugin(String disableReason) {
+        getLogger().info("Plugin disable: " + disableReason);
+        getServer().getPluginManager().disablePlugin(this);
     }
 }
