033f411baeac83a7ae5c311cade7f9d514e7e279
owoPeef <owopeef@gmail.com>
2022-01-09 03:34:52 +0300

    Добавлена поддержка команд

--- src/ru/owopeef/chatandtabmanager/Main.java
+++ src/ru/owopeef/chatandtabmanager/Main.java
@@ -1,14 +1,31 @@
 package ru.owopeef.chatandtabmanager;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.owopeef.chatandtabmanager.utils.Config;
+
+import java.util.Map;
 
 public class Main extends JavaPlugin
 {
     @Override
     public void onEnable()
     {
-        Config.loadConfig();
-        getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
+        PluginManager pm = getServer().getPluginManager();
+        if (pm.isPluginEnabled("PermissionsEx") && pm.isPluginEnabled("PlaceholderAPI")) {
+            Config.loadConfig();
+            getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
+            for (Map.Entry<String, Map<String, Object>> cmd : getDescription().getCommands().entrySet()) {
+                String command = cmd.getKey();
+                try {
+                    getCommand(command).setExecutor(new Commands());
+                    this.getLogger().info("Command \"" + command + "\" init success");
+                } catch (Exception exc) {
+                    this.getLogger().warning("Command \"" + command + "\" don't init. ("+exc.getMessage()+")");
+                }
+            }
+        } else {
+            this.setEnabled(false);
+        }
     }
 }
